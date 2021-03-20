dbf8b4ddfe4df5f7d9f5beaad23ee70302fb2d9e
Vladislav Mikitich <vmikitich@gmail.com>
2021-03-20 20:18:20 +0300

    remove cerberus-spicific code

--- response_test.go
+++ response_test.go
@@ -1,15 +1,12 @@
 package api
 
 import (
-	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/Decentr-net/cerberus/pkg/api"
 
 	"github.com/Decentr-net/go-api/test"
 )
@@ -53,36 +50,4 @@ func Test_WriteInternalError(t *testing.T) {
 	assert.Greater(t, len(b.String()), 20) // stacktrace
 	assert.True(t, strings.Contains(b.String(), "some error"))
 	assert.JSONEq(t, `{"error":"internal error"}`, w.Body.String())
-}
-
-func Test_WriteVerifyError(t *testing.T) {
-	t.Run("bad request", func(t *testing.T) {
-		b, w, r := test.NewAPITestParameters(http.MethodGet, "", nil)
-
-		WriteVerifyError(r.Context(), w, api.ErrInvalidPublicKey)
-
-		assert.Empty(t, b.String())
-		assert.Equal(t, http.StatusBadRequest, w.Code)
-		assert.JSONEq(t, `{"error":"invalid request: public key is invalid"}`, w.Body.String())
-	})
-
-	t.Run("not verified", func(t *testing.T) {
-		b, w, r := test.NewAPITestParameters(http.MethodGet, "", nil)
-		WriteVerifyError(r.Context(), w, api.ErrNotVerified)
-
-		assert.Empty(t, b.String())
-		assert.Equal(t, http.StatusUnauthorized, w.Code)
-		assert.JSONEq(t, `{"error":"failed to verify message"}`, w.Body.String())
-	})
-
-	t.Run("internal error", func(t *testing.T) {
-		b, w, r := test.NewAPITestParameters(http.MethodGet, "", nil)
-
-		WriteVerifyError(r.Context(), w, errors.New("some error"))
-
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-		assert.Greater(t, len(b.String()), 20) // stacktrace
-		assert.True(t, strings.Contains(b.String(), "some error"))
-		assert.JSONEq(t, `{"error":"internal error"}`, w.Body.String())
-	})
 }
